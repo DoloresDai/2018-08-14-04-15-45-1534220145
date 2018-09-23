5920aabeb3459a50804d73ae61ca4dd280c9a2f6
DoloresDai <43499069+DoloresDai@users.noreply.github.com>
2018-09-23 17:14:17 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,5 +1,7 @@
 // Write your cade below:
-var a = 5;
+module.exports = function main() {
+  var a = 5;
 var b = 2;
-alert(parseInt(a/b));//整数 parseInt()丢弃小数部分,保留整数部分
-alert(a%b);//余数
+alert(parseInt(a/b));
+alert(a%b);
+}; 
